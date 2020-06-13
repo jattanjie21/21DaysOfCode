c0cc3950d5715fca1e85509f591dab2e55a7cbd7
jatta-njie21 <laboyjatta@gmail.com>
2020-06-13 17:29:40 +0000

    day2.0

---
+++ PythonChallenges/Day2/day2.py
@@ -0,0 +1,39 @@
+# Why not try a loop today....
+#
+#
+# What’s the minimum number of times you have to flip a coin before you can have
+# three consecutive flips that result in the same outcome (either all three are heads or
+# all three are tails)? What’s the maximum number of flips that might be needed? How
+# many flips are needed on average? 
+#
+#  
+# In this exercise i will explore these questions
+# by creating a program that simulates several series of coin flips.
+
+
+#Just realized i need a method i created yesterday
+#Awesome so i just have to import sweet!!
+from Day1.List import *
+
+import random
+minimum = 1
+maximum = 2
+# the number 1 means head 
+# the number 2 means tail
+
+flip_test = 10
+# Number of times i will test the script sort of
+# Inorder to find the average
+
+list_flips = []
+
+def flip():
+    if list_flips.is_empty():
+        random.randint(minimum,maximum).append(list_flips)
+    else:
+        
+
+
+
+
+
