08eb841b99bf641c20704f8c9bb41673f46f55fa
jatta-njie21 <laboyjatta@gmail.com>
2020-06-14 14:50:49 +0000

    day3.0

--- PythonChallenges/Day 3/day3.py
+++ PythonChallenges/Day 3/day3.py
@@ -5,6 +5,31 @@
 #tell them their guess is either too high, or too low. 
 #This will get you started with the random library if you haven't already used it.
 
+import random
+
+def number_guesser():
+    tries = int(input("How many tries do you need incase? "))
+    my_guess = int(input("Guess a "))
+    guesses = 0
+
+    while guesses <= tries:
+        random_number = random.randrange(0,21)
+        if random_number == my_guess:
+            print("Bravo!!!!!!!!")
+            break
+        elif random_number < my_guess:
+            print(guesses,"attempt wrong, Guess was too low")
+        elif random_number > my_guess:
+            print(guesses,"attempt wrong, Guess was too high")
+        else:
+            print()
+
+        guesses += 1
+
+    print("END OF PROGRAM!!!!!!")
+
+
+
 
 
 
