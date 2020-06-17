326908e47a300c62cf7e67e6eebb3c570208e5cb
jatta-njie21 <laboyjatta@gmail.com>
2020-06-17 17:39:04 +0000

    challenge 6

---
+++ Daily Python/Day 6/day6.py
@@ -0,0 +1,9 @@
+# In this challenge we will write a program to convert an angle
+#  from degrees toraidans or from radians to degrees based on 
+# the user’s choice. Our program will:
+
+#    Display a menu of options to the end user.
+#    Ask the end-user to choose an option from the menu.
+#    Ask the end-user to input an angle in the required unit.
+#    Calculate and output the angle in the alternative angle unit.
+
