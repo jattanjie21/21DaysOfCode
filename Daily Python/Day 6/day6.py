e0e2f30aa1504ea9ad2db47449684609c025eccd
jatta-njie21 <laboyjatta@gmail.com>
2020-06-17 18:22:06 +0000

    FUNCTIONS

--- Daily Python/Day 6/day6.py
+++ Daily Python/Day 6/day6.py
@@ -7,11 +7,20 @@
 #    Ask the end-user to input an angle in the required unit.
 #    Calculate and output the angle in the alternative angle unit.
 
-def rad_deg():
-    pass
+import math
 
-def deg_rad():
-    pass
+def rad_deg(number):
+    degree = number * ( 180 / math.pi )
+    return degree
 
-def input():
-    pass
+def deg_rad(number):
+    radian = number * (math.pi / 180)
+    return radian
+
+def choice():
+    print("MENU")
+    print("A. CONVERT FROM DEGREE TO RADIAN")
+    print("B. CONVERT FROM RADIAN TO DEGREES")
+    print("C. QUIT")
+
+    choice = input(float(">> "))
