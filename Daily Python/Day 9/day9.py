f6c4035a461535209096db31f0996278830eb202
jatta-njie21 <laboyjatta@gmail.com>
2020-06-20 17:43:06 +0000

    create table function

--- Daily Python/Day 9/day9.py
+++ Daily Python/Day 9/day9.py
@@ -2,3 +2,22 @@
 #
 #
 # CONNECTING TO A DATABASE LOCALLY AND ALSO MANIPULATING DATA VIA A SCRIPT
+
+import sqlite3
+
+#creating a connection
+conn = sqlite3.connect('day9.db')
+
+#creating cursor
+cur = conn.cursor()
+
+#Function to create table
+def create_table(table_name):
+    cur.execute('CREATE TABLE IF NOT EXISTS ?',(table_name))
+    #if not exists so you can avoid deleting a previous table
+    conn.commit()
+    conn.close()
+
+    #always close after a commit
+
+
