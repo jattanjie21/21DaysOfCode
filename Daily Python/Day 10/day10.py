d53e0d5e0948e787cfa7248f62df51502c0715d3
jatta-njie21 <laboyjatta@gmail.com>
2020-06-21 23:49:26 +0000

    add function

--- Daily Python/Day 10/day10.py
+++ Daily Python/Day 10/day10.py
@@ -8,5 +8,7 @@ table = PrettyTable()
 #Creating field names
 table.field_names = ['Name', 'Age', 'Address']
 
+
+#Function that adds data to the table
 def add_record(name,age,address):
     table.add_row([name, age, address])
