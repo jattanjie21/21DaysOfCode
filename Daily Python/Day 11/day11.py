6b7ad83833282e16aa8b73a1466a5bf455378e0f
jatta-njie21 <laboyjatta@gmail.com>
2020-06-22 19:24:11 +0000

    commit10

--- Daily Python/Day 11/day11.py
+++ Daily Python/Day 11/day11.py
@@ -1,4 +1,6 @@
-
+"""
+A simple script and library to convert files or strings from dos like
+line endings with Unix like line endings.
 """
 
 import argparse
@@ -6,7 +8,7 @@ import os
 
 
 def str2unix(input_str: str) -> str:
-    r"""
+    """
     Converts the string from \r\n line endings to \n
 
     Parameters
@@ -33,8 +35,7 @@ def dos2unix(source_file: str, dest_file: str):
     dest_file
         The path to the converted file for output
     """
-    # NOTE: Could add file existence checking and file overwriting
-    # protection
+
     with open(source_file, 'r') as reader:
         dos_content = reader.read()
 
@@ -44,40 +45,3 @@ def dos2unix(source_file: str, dest_file: str):
         writer.write(unix_content)
 
 
-if __name__ == "__main__":
-    # Create our Argument parser and set its description
-    parser = argparse.ArgumentParser(
-        description="Script that converts a DOS like file to an Unix like file",
-    )
-
-    # Add the arguments:
-    #   - source_file: the source file we want to convert
-    #   - dest_file: the destination where the output should go
-
-    # Note: the use of the argument type of argparse.FileType could
-    # streamline some things
-    parser.add_argument(
-        'source_file',
-        help='The location of the source '
-    )
-
-    parser.add_argument(
-        '--dest_file',
-        help='Location of dest file (default: source_file appended with `_unix`',
-        default=None
-    )
-
-    # Parse the args (argparse automatically grabs the values from
-    # sys.argv)
-    args = parser.parse_args()
-
-    s_file = args.source_file
-    d_file = args.dest_file
-
-    # If the destination file wasn't passed, then assume we want to
-    # create a new file based on the old one
-    if d_file is None:
-        file_path, file_extension = os.path.splitext(s_file)
-        d_file = f'{file_path}_unix{file_extension}'
-
-    dos2unix(s_file, d_file)
