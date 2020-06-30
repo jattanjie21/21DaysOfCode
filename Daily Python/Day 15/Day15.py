52e5cd285989fa86bd65aa663baf017b614333ba
Modou Lamin Jatta <laboyjatta@gmail.com>
2020-06-30 23:48:06 +0100

    Update Day15.py

--- Daily Python/Day 15/Day15.py
+++ Daily Python/Day 15/Day15.py
@@ -25,3 +25,17 @@ def is_palindrome(num):
         return num
     else:
         return False
+
+import random
+
+#lottery function
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield random.randint(1, 40)
+
+    # returns a 7th number between 1 and 15
+    yield random.randint(1,15)
+
+for random_number in lottery():
+       print("And the next number is... %d!" %(random_number))
