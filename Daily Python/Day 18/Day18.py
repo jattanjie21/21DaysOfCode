eed0bed4083cf560f88f693bde76a8680543cf39
Modou Lamin Jatta <laboyjatta@gmail.com>
2020-07-05 19:51:20 +0100

    Update Day18.py

--- Daily Python/Day 18/Day18.py
+++ Daily Python/Day 18/Day18.py
@@ -1,1 +1,17 @@
+import numpy as np
+
+# Creating arrays
+a = np.array([0, 1, 2, 3])
+
+# Two by three array
+b = np.array([[0, 1, 2], [3, 4, 5]])
+
+a = np.random.rand(4)       # uniform in [0, 1]
+
+
+#Finding the array type
+a.dtype
+
+#Indexing and slicing
+a = np.arange(10)
 
